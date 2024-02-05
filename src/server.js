bf8e65bda50f27946a973ea761f77b376d364bf4
adityas-opse <adityakushinagar123@gmail.com>
2024-02-06 00:51:53 +0530

    add prettier

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
-console.log("hello aditya ")
+import express from "express";
+
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("server is running ");
+});
+
+app.listen(4000, () => {
+  console.log("server is running on port 4000");
+});
